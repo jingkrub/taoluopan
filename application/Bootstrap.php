7d4b9a6ffffd8f21c2d23ea7c9de3aef3e765bdc
flyw <ecmpc@qq.com>
2012-06-19 15:38:02 +0700

    renew bootstrap

--- application/Bootstrap.php
+++ application/Bootstrap.php
@@ -11,25 +11,29 @@ class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
 
 	protected function _initConfig()
 	{
-		$config = new Zend_Config($this->getOptions());
-		
-		$db = Zend_Db::factory($config->resources->db->adapter , $config->resources->db->params );
-		Zend_Db_Table::setDefaultAdapter($db);
-		$db->query('SET names "utf8"');
-		
-		Zend_Registry::set('config', $config);
-		return $config;
-		
-		
+		Zend_Registry::set('config', new Zend_Config($this->getOptions()));		
+	}
+	
+	protected function _initDatabases()
+	{
+	    $resource = $this->getPluginResource('db');
+	    $db = $resource->getDbAdapter();
+	    $db->query('SET names "utf8"');
+// 	    Zend_Db_Table::setDefaultAdapter($db);
+	}
+	
+	protected function _initDate()
+	{
+	    date_default_timezone_set(Zend_Registry::get('config')->settings->application->timezone);
 	}
 
-	protected function _initAutoload()
-	{
-		$moduleLoader = new Zend_Application_Module_Autoloader(array(
-			'namespace' => 'Ecmpc',
-			'basePath' => APPLICATION_PATH));
-		//$moduleLoader->addResourceType('Validator' , 'Validator' , 'Validator');
-		return $moduleLoader;
-	}
+// 	protected function _initAutoload()
+// 	{
+// 		$moduleLoader = new Zend_Application_Module_Autoloader(array(
+// 			'namespace' => 'Ecmpc',
+// 			'basePath' => APPLICATION_PATH));
+// 		//$moduleLoader->addResourceType('Validator' , 'Validator' , 'Validator');
+// 		return $moduleLoader;
+// 	}
 }
 
