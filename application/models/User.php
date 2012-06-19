03ec7fb4ae7ed1f74d7bbdd02459ca0701153029
flyw <ecmpc@qq.com>
2012-06-19 15:41:06 +0700

    fix: timestamp

--- application/models/User.php
+++ application/models/User.php
@@ -6,15 +6,17 @@ class Ecmpc_Model_User
 		$user = new Ecmpc_Model_DbTable_User();
 		$userInfo = $user->sqlGetUser($authNamespace->userId);
 		
+		$ts = date("Y-m-d H:i:s" , $authNamespace->timestamp);
+		
 		if (null == $userInfo)
 		{
 			$user->sqlInsert($authNamespace->userId, $authNamespace->nick, $authNamespace->topSession, 
-						  $authNamespace->expiresIn, $authNamespace->reExpiresIn, $authNamespace->timestamp );
+						  $authNamespace->expiresIn, $authNamespace->reExpiresIn, $ts );
 		}
 		else
 		{
 			$user->sqlUpdate($authNamespace->userId, $authNamespace->nick, $authNamespace->topSession, 
-						  $authNamespace->expiresIn, $authNamespace->reExpiresIn, $authNamespace->timestamp );
+						  $authNamespace->expiresIn, $authNamespace->reExpiresIn, $ts );
 		}
 		
 	}	
