d62cc4f8e2b73a85cbc9d2b158be6df5c5e54da4
flyw <ecmpc@qq.com>
2012-06-19 12:20:25 +0700

    init

--- application/models/User.php
+++ application/models/User.php
@@ -1,8 +1,21 @@
 <?php
-
-class Application_Model_User
+class Ecmpc_Model_User
 {
-
-
+	public function saveUserSignIn($authNamespace)
+	{		
+		$user = new Ecmpc_Model_DbTable_User();
+		$userInfo = $user->sqlGetUser($authNamespace->userId);
+		
+		if (null == $userInfo)
+		{
+			$user->sqlInsert($authNamespace->userId, $authNamespace->nick, $authNamespace->topSession, 
+						  $authNamespace->expiresIn, $authNamespace->reExpiresIn, $authNamespace->timestamp );
+		}
+		else
+		{
+			$user->sqlUpdate($authNamespace->userId, $authNamespace->nick, $authNamespace->topSession, 
+						  $authNamespace->expiresIn, $authNamespace->reExpiresIn, $authNamespace->timestamp );
+		}
+		
+	}	
 }
-
