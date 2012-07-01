0c5850fb08a293418f1a5f198442da65871910be
flyw <ecmpc@qq.com>
2012-07-01 18:25:56 +0700

    验证队列的用法实例。

--- application/controllers/NavoController.php
+++ application/controllers/NavoController.php
@@ -6,7 +6,7 @@ class NavoController extends Zend_Controller_Action
     public function init()
     {
         /* Initialize action controller here */
-        if (false == Navo_Model_Authentication::valid() )
+        if (false == Navo_Service_Authentication::valid() )
         {
         	$this->_helper->redirector('index' , 'user');
         }
@@ -14,14 +14,14 @@ class NavoController extends Zend_Controller_Action
 
     public function indexAction()
     {
-        $user = Navo_Model_Authentication::getUser();
+        $user = Navo_Service_Authentication::getUser();
         
        //获取user的所有 item
        
        $item = new Navo_Model_DbTable_Item();
        $itemRowset = $item->sqlGetAllItemByUser( $user->getId() );
        
-       $slotContainer = new Navo_Model_SlotContainer( $itemRowset->count() );
+       $slotContainer = new Navo_Service_SlotContainer( $itemRowset->count() );
        
        foreach ($itemRowset as $item) {
            $slotContainer->setItem($item);
@@ -29,6 +29,47 @@ class NavoController extends Zend_Controller_Action
        $slotContainer->reAllocate();
        
        exit;
+    }
+    
+    public function validationAction()
+    {
+        //TODO: Fetch user_id.
+        
+        //取出所有的user
+        
+        //按照每一个user 取出可以改动的item
+        
+        //取出可以改动的时间表。
+        
+        $userId = '13424324';
+        $user = new Navo_Model_User($userId);
+        
+        $userValidatorChain = new Navo_Service_Validate();
+        $userValidatorChain->addValidator( new Navo_Service_Validate_User_IsActived() )
+        ->addValidator(new Navo_Service_Validate_User_ShopUpdated());
+        
+        if ($userValidatorChain->isValid($user)) {
+        	//TODO: contune;
+        }
+        ///////////////////////////////////////////////////////////////////////
+        
+        $item = new Navo_Model_DbTable_Item();
+        //TODO: Fetch num_iid.
+        $itemRow = $item->sqlGetItem('num_iid');
+        
+        
+        // Create a validator chain and add validators to it
+        $itemValidatorChain = new Navo_Service_Validate();
+        $itemValidatorChain->addValidator( new Navo_Service_Validate_Item_Violation() )
+        			    ->addValidator( new Navo_Service_Validate_Item_SecondKill() )
+        				->addValidator( new Navo_Service_Validate_Item_FrequentUpdate());
+        
+        if ($itemValidatorChain->isValid($itemRow)) {
+        	//TODO: contune;
+        } 
+        
+        
+        
     }
 
 
